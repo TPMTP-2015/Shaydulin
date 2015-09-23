7d131b3abdce1109054653f5d2255656567cd5cf
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-09-23 12:58:56 +0300

    Initial Commit

---
+++ ArraySum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArraySum
+//
+//  Created by Руслан on 23.09.15.
+//  Copyright © 2015 MIPT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
