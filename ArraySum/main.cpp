0ee343113fa1a7dec0f343179bae726abd38726c
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-09-23 18:36:15 +0300

    comehow everything is broken again

--- ArraySum/main.cpp
+++ ArraySum/main.cpp
@@ -9,27 +9,32 @@
 #include <iostream>
 #include "ArrayHandler.hpp"
 #include <pthread.h>
+#include <time.h>
 using std::cout;
 
-#define RS_ARRAY_SIZE 100000000
-#define RS_THREAD_NUMBER 4
+#define RS_ARRAY_SIZE 480000000
+
 
 int main(int argc, const char * argv[]) {
     int* hugeArray = ArrayHandler::generateRandomArray(RS_ARRAY_SIZE);
+    clock_t startTime, endTime;
     pthread_t threads[4];
-//    for (int  i = 0; i < 4; i++) {
-    for(int i = 0; i < RS_THREAD_NUMBER; i++){
-        ArrayWrapper* arg = new ArrayWrapper;
-        arg->array = &hugeArray[i*RS_ARRAY_SIZE/RS_THREAD_NUMBER];
-        arg->size = RS_ARRAY_SIZE/RS_THREAD_NUMBER;
-        pthread_create(&threads[i], NULL, &ArrayHandler::sumArray, arg);
+    for (int threadNum = 1; threadNum <=4; threadNum++) {
+        startTime = clock();
+        for(int i = 0; i < threadNum; i++){
+            ArrayWrapper* arg = new ArrayWrapper;
+            arg->array = &hugeArray[i*RS_ARRAY_SIZE/threadNum];
+            arg->size = RS_ARRAY_SIZE/threadNum;
+            pthread_create(&threads[i], NULL, &ArrayHandler::sumArray, arg);
+        }
+        long long sum = 0;
+        void* tmp;
+        for (int i = 0; i < threadNum; i++) {
+            pthread_join(threads[i], &tmp);
+            sum += *(long long*) tmp;
+        }
+        endTime = clock();
+        cout << "The sum is " << sum << " Time for " << threadNum << " threads is " << (endTime - startTime) <<"\n";
     }
-    long long sum = 0;
-    void* tmp;
-    for (int i = 0; i < RS_THREAD_NUMBER; i++) {
-        pthread_join(threads[i], &tmp);
-        sum += *(long long*) tmp;
-    }
-    cout << "The sum is " << sum << "\n";
     return 0;
 }
