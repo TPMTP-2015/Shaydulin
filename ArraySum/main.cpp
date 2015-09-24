44bf5e2cd65b263332137bcee6e82dfa5cd27a31
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-09-24 14:41:24 +0300

    added omp

--- ArraySum/main.cpp
+++ ArraySum/main.cpp
@@ -9,14 +9,17 @@
 #include <iostream>
 #include "ArrayHandler.hpp"
 #include "TestsRunner.hpp"
+#include <libiomp/omp.h>
 using std::cout;
 
 #define RS_ARRAY_SIZE 720000000
 
 
 int main(int argc, const char * argv[]) {
-    TestsRunner::runPOSIXThreadsTests(RS_ARRAY_SIZE, 1);
-    TestsRunner::runPOSIXThreadsTests(RS_ARRAY_SIZE, 2);
-    TestsRunner::runPOSIXThreadsTests(RS_ARRAY_SIZE, 4);
+//    TestsRunner::runPOSIXThreadsTests(RS_ARRAY_SIZE, 1);
+//    TestsRunner::runPOSIXThreadsTests(RS_ARRAY_SIZE, 2);
+//    TestsRunner::runPOSIXThreadsTests(RS_ARRAY_SIZE, 4);
+#pragma omp parallel
+    printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
     return 0;
 }
